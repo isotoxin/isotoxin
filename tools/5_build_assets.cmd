0ac2aace46c37c7d9031717ca42c9d78552a1c3a
Rotkaermota <isotoxin.dev@yandex.ru>
2015-10-03 18:05:42 +0300

    assets build script, optimized smiles png

---
+++ tools/5_build_assets.cmd
@@ -0,0 +1,28 @@
+@echo off
+SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
+
+md ..\bin
+rd /S /Q ..\bin\~assets
+md ..\bin\~assets
+md ..\bin\~assets\smiles
+
+cd ..\assets\smiles
+
+for /f %%i in ('"dir * /A:D /B"') do ( 
+cd %%i
+..\..\..\tools\7z a -tzip -r -mx=9 ..\..\..\bin\~assets\smiles\s.zip
+cd ..
+ren ..\..\bin\~assets\smiles\s.zip %%i.zip
+)
+
+cd ..
+
+xcopy .\loc ..\bin\~assets\loc /s /e /y /i
+xcopy .\themes ..\bin\~assets\themes /s /e /y /i
+xcopy .\sounds ..\bin\~assets\sounds /s /e /y /i
+copy .\*.template ..\bin\~assets\
+
+cd ..\bin\~assets
+..\..\tools\7z a -tzip -r -mx=9 ..\isotoxin.data
+cd ..
+rd /S /Q ~assets
