6b22a38d01c77020e626f1fabb85c417d758257f
Rotkaermota <isotoxin.dev@yandex.ru>
2016-05-09 19:08:29 +0300

    build problem fix

--- build-win/3-build-isotoxin-2015.cmd
+++ build-win/3-build-isotoxin-2015.cmd
@@ -5,6 +5,11 @@ set msb="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
 set GARBAGE=%cd%\..\~garbage
 md %GARBAGE%
 
+md ..\tools
+copy kill_crap.cmd ..\tools
+copy prebuild.cmd ..\tools
+
+
 if a%1a == a64a (
 %msb% ..\source\isotoxin.2015.64.sln /fl2 /clp:ErrorsOnly;PerformanceSummary;Summary /m:3 /t:Rebuild /p:Configuration=Final;Platform=x64;GARBAGE=%GARBAGE%
 ) else (
